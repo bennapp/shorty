606d6aec26c6a5f5a22ac78b2865b9db324c1b5f
Ben Nappier <ben@mavenlink.com>
2015-10-22 12:46:38 -0700

    reduce routing to new, show and create

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -1,6 +1,4 @@
 class LookupsController < ApplicationController
-  before_action :set_lookup, only: [:show, :edit, :update, :destroy]
-
   def lookup
     url = Lookup.where('token = ?', request.params['path']).first.try(:url)
 
@@ -13,15 +11,8 @@ class LookupsController < ApplicationController
     redirect_to url
   end
 
-  # GET /lookups
-  # GET /lookups.json
-  def index
-    @lookups = Lookup.all
-  end
-
-  # GET /lookups/1
-  # GET /lookups/1.json
   def show
+    @lookup = Lookup.where('token = ?', params["token"]).first
   end
 
   # GET /lookups/new
@@ -29,18 +20,14 @@ class LookupsController < ApplicationController
     @lookup = Lookup.new
   end
 
-  # GET /lookups/1/edit
-  def edit
-  end
-
-  # POST /lookups
-  # POST /lookups.json
   def create
+    mode = params[:mode]
     @lookup = Lookup.new(lookup_params)
+    @lookup.token = @lookup.shortify if mode == 'shortify'
 
     respond_to do |format|
       if @lookup.save
-        format.html { redirect_to @lookup, notice: 'Lookup was successfully created.' }
+        format.html { redirect_to "/lookup/#{@lookup.token}" }
         format.json { render :show, status: :created, location: @lookup }
       else
         format.html { render :new }
@@ -49,38 +36,9 @@ class LookupsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lookups/1
-  # PATCH/PUT /lookups/1.json
-  def update
-    respond_to do |format|
-      if @lookup.update(lookup_params)
-        format.html { redirect_to @lookup, notice: 'Lookup was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lookup }
-      else
-        format.html { render :edit }
-        format.json { render json: @lookup.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /lookups/1
-  # DELETE /lookups/1.json
-  def destroy
-    @lookup.destroy
-    respond_to do |format|
-      format.html { redirect_to lookups_url, notice: 'Lookup was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lookup
-      @lookup = Lookup.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def lookup_params
-      params.require(:lookup).permit(:token, :url)
-    end
+  def lookup_params
+    params.permit(:token, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :lookups
+  resources :lookups, only: [:create]
+  get 'lookup/:token' => 'lookups#show'
+  post 'lookups', to: 'lookups#create'
+  root 'lookups#new'
   get '*path', to: 'lookups#lookup'
 end
