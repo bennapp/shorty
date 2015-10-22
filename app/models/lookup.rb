398a90d6741710ac58f26ef58840ae3be3ccb6b1
Ben Nappier <ben@mavenlink.com>
2015-10-22 12:44:41 -0700

    lookups can shortify and generate random token

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,12 +1,35 @@
 class Lookup < ActiveRecord::Base
+  include Dictionary
+
   validates_presence_of :url
   validates_presence_of :token
   validates_uniqueness_of :token
+  #validate TODO: validate lenght of token make sure its no over 255
 
   before_save :prepend_http_protocol
   before_update :prepend_http_protocol
 
+  before_validation :base_36_encode
+  before_update :base_36_encode
+
+  def shortify
+    loop do
+      adj = rand_adj
+      next_adj = different_rand_adj(adj)
+      word = [adj, next_adj, rand_noun].join('-')
+      break word unless Lookup.where(token: word).first.present?
+    end
+  end
+
   def prepend_http_protocol
     self.url = "http://#{url}" unless url.index('http://') == 0 || url.index('https://') == 0
+  end
+
+  def base_36_encode
+    return if token.present?
+    self.token = loop do
+      random_id = rand(('z' * 8).to_i(36))
+      break random_id.to_s(36) unless Lookup.where(token: random_id.to_s(36)).first.present?
+    end
   end
 end
--- spec/models/lookup_spec.rb
+++ spec/models/lookup_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Lookup, :type => :model do
-  let(:lookup) { lookups(:lookie) }
+  let(:lookup) { lookups(:lookup) }
 
   describe 'validations' do
     before do
@@ -11,11 +11,6 @@ RSpec.describe Lookup, :type => :model do
 
     it 'requires presence of url' do
       lookup.url = nil
-      expect(lookup.valid?).to be_falsey
-    end
-
-    it 'requires presence of token' do
-      lookup.token = nil
       expect(lookup.valid?).to be_falsey
     end
 
@@ -44,6 +39,38 @@ RSpec.describe Lookup, :type => :model do
       it 'prepends the url with http' do
         lookup = Lookup.create!(url: 'www.google.com', token: 'foo')
         expect(lookup.url == 'http://www.google.com').to be_truthy
+      end
+    end
+  end
+
+  describe 'token' do
+    context 'present' do
+      it 'sets input' do
+        token_before = lookup.token
+        lookup.save!
+        expect(lookup.reload.token).to eq(token_before)
+      end
+    end
+
+    context 'not present' do
+      context 'next id is not taken' do
+        it 'sets base_36 encoded random int' do
+          lookup.token = nil
+          lookup.save!
+          lookup.reload.token.size.should be < 9
+        end
+      end
+
+      context 'next id is taken' do
+        before do
+          Lookup.create!(token: Lookup.maximum(:id).next.to_s(36), url: 'www.dgaf.net')
+        end
+
+        it 'sets base_36 encoded random int' do
+          lookup.token = nil
+          lookup.save!
+          lookup.reload.token.size.should be < 9
+        end
       end
     end
   end
